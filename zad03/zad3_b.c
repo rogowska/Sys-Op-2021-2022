3304008de5e0f9037705e7aa1eecb306b20aa288
rogowska <rogowska.oliwia@gmail.com>
2022-03-25 21:56:14 +0100

    changes at zad3_b

--- zad03/zad3_b.c
+++ zad03/zad3_b.c
@@ -2,30 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
-main(argc, argv[])
+int main(int argc, char *argv[])
 {
+    if (argc != 3)
+    {
+        printf("Wrong number of arguments (expected: signal number and action number)\n");
+        exit(EXIT_FAILURE);
+    }
 
-    int id, childpid;
-    int signumber = strtol(argv[1], NULL, 0);
+    int id;
+    char *signumber_str = argv[1];
+    char *sigaction_str = argv[2];
+    int signumber_int = strtol(argv[1], NULL, 0);
 
     id = fork();
 
     if (id > 0)
     {
         sleep(2);
-        kill(childpid, signumber);
+        kill(id, signumber_int);
     }
-    if (id == 0)
+    else if (id == 0)
     {
-        execlp("./zad3_a.x", "zad3_a.x", NULL);
+        execlp("./zad3_a.x", "zad3_a.x", signumber_str, sigaction_str, NULL);
         perror("execlp error");
-        childpid = getpid();
     }
     else
     {
         perror("fork error\n");
         exit(EXIT_FAILURE);
     }
-    return 0;
+    exit(EXIT_SUCCESS);
 }
