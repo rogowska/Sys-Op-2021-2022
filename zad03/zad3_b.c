6329ade27fdf8f56a0229447971b925ceb079529
rogowska <rogowska.oliwia@gmail.com>
2022-03-26 22:39:18 +0100

    comments, README, MAKEFILE

--- zad03/zad3_b.c
+++ zad03/zad3_b.c
@@ -18,19 +18,23 @@ int main(int argc, char *argv[])
     char *sigaction_str = argv[2];
     int signumber_int = strtol(argv[1], NULL, 0);
 
+    /*tworzenie procesu macierzystego i potomnego*/
     id = fork();
 
     if (id > 0)
     {
+        /*oczekiwanie na uruchomienie procesu potomnego*/
         while ((kill(id, 0) == -1) || (errno == ESRCH))
         {
             errno = ETIME;
         }
         sleep(1);
+        /*wysłanie sygnału do procesu potomnego*/
         kill(id, signumber_int);
     }
     else if (id == 0)
     {
+        /*uruchomienie programu obsługi sygnałów w procesie potomnym*/
         execlp("./zad3_a.x", "zad3_a.x", signumber_str, sigaction_str, NULL);
         perror("execlp error");
     }
--- zad03/zad3_c.c
+++ zad03/zad3_c.c
@@ -9,50 +9,76 @@
 int main(int argc, char *argv[])
 {
     int i, id;
+    const int proc_nb = 3;
 
     if (argc != 3)
     {
         printf("Wrong number of arguments (expected: signal number and action number)\n");
         exit(EXIT_FAILURE);
     }
+    char errmsg[20];
     char *signumber_str = argv[1];
     char *sigaction_str = argv[2];
     int signumber_int = strtol(argv[1], NULL, 0);
+
+    /*tworzenie procesu macierzystego i potomnego*/
     id = fork();
 
+    
     if (id > 0)
     {
         printf("Identyfikator procesu: %d\n", getpid());
         printf("Identyfikator procesu macierzystego: %d\n", getppid());
         printf("Identyfikator grupy procesów: %d\n\n", getpgrp());
+        /*sprawdzenie istnienia procesu potomnego*/
         while ((kill(id, 0) == -1) || (errno == ESRCH))
         {
             errno = ETIME;
         }
         sleep(1);
-        kill(id, signumber_int);
+        /*wysyłanie sygnałów do grupy procesów potomnych*/ 
+        for (i = 0; i < proc_nb; i++)
+        {
+            sleep(1);
+            kill(-id, signumber_int);
+        }
         wait(NULL);
     }
     else if (id == 0)
     {
+        /*ustawienie procesu potomnego jako lidera grupy*/
         setpgid(getpid(), getpid());
+        /*obsługa sygnału - ignorowanie przez lidera grupy*/
+        if (signal(signumber_int, SIG_IGN) == SIG_ERR)
+        {
+            sprintf(errmsg, "Signal %d%s", signumber_int, " error");
+            perror(errmsg);
+            exit(EXIT_FAILURE);
+        }
         printf("Identyfikator procesu: %d\n", getpid());
         printf("Identyfikator procesu macierzystego: %d\n", getppid());
         printf("Identyfikator grupy procesów: %d\n\n", getpgrp());
-        for (i = 0; i < 2; i++)
+        /*tworzenie procesów potomnych lidera grupy*/
+        for (i = 0; i < proc_nb; i++)
         {
             int id2 = fork();
-            wait(NULL);
+            /*oczekiwanie na zakończenie procesów potomnych i wyświetlenie ich statusu*/
+            int waitVal = wait(NULL);
+            if (waitVal != -1)
+            {
+                printf("Wait return value: %d\n", waitVal);
+            }
             if (id2 > 0)
             {
             }
             else if (id == 0)
             {
-                execlp("./zad3_a.x", "zad3_a.x", signumber_str, sigaction_str, NULL);
-                perror("execlp error");
                 printf("Identyfikator procesu: %d\n", getpid());
                 printf("Identyfikator procesu macierzystego: %d\n", getppid());
                 printf("Identyfikator grupy procesów: %d\n\n", getpgrp());
+                /*uruchomnienie programu obsługi sygnałów*/
+                execlp("./zad3_a.x", "zad3_a.x", signumber_str, sigaction_str, NULL);
+                perror("execlp error");
             }
         }
     }
